679d80aac531a1c830334054f7fa7c66baa1e7bf
Valentin <valentin_github@abv.bg>
2017-11-02 18:13:25 +0200

    Add Exercises: ASP.NET Core Introduction

---
+++ 09.C# MVC Frameworks - ASP.NET Core/01.ASP.NET Core Introduction/FluffyDuffyMunchkinCats/Data/CatsDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace FluffyDuffyMunchkinCats.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class CatsDbContext : DbContext
+    {
+        public CatsDbContext(DbContextOptions<CatsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
