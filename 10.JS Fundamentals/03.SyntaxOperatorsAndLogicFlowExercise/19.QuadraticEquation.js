0e9a6d6039d4a6bdb19efaebe11db3d6049cdf17
Valentin <obbittoo@gmail.com>
2018-04-23 23:02:53 +0300

    Task 19 / 21

--- 10.JS Fundamentals/03.SyntaxOperatorsAndLogicFlowExercise/19.QuadraticEquation.js
+++ 10.JS Fundamentals/03.SyntaxOperatorsAndLogicFlowExercise/19.QuadraticEquation.js
@@ -1,5 +1,22 @@
 function quadraticEquation(input){
-    let [a, b, c] = input;
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+    let c = Number(input[2]);
 
     let descriminant = b ** 2 - 4 * a * c;
+
+    if (descriminant > 0){
+        let x1 = (-b + Math.sqrt(descriminant)) / (2 * a);
+        let x2 = (-b - Math.sqrt(descriminant)) / (2 * a);
+
+        console.log(Math.min(x1, x2));
+        console.log(Math.max(x1, x2));
+    } else if (descriminant === 0){
+        console.log(-b / (2 * a));
+    } else if (descriminant < 0){
+        console.log('No');
+    }     
 }
+
+quadraticEquation([6, 11, -35]);
+quadraticEquation([1, -12, 36]);
